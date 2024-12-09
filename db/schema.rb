2a3a414e5895bfb3db6c0b98de167ed0ccd097aa
Milena Arms <183767485+The-Milena@users.noreply.github.com>
2024-12-09 19:58:55 +0100

    simple-airbnb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_13_152309) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_09_163336) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_13_152309) do
     t.integer "number_of_guests"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,21 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Flat.destroy_all
+
+puts "Taking away old seed"
+
+10.times do Flat.create!(
+  name: Faker::Book.title,
+  address: Faker::Address.full_address,
+  description: Faker::TvShows::GameOfThrones.quote,
+  price_per_night: Random.new.rand(50..350),
+  number_of_guests: Random.new.rand(1..20),
+  picture_url: 'https://images.unsplash.com/photo-1519302959554-a75be0afc82a?q=80&w=2880&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  )
+end
+
+puts "created #{Flat.count} new flats"
